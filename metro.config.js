8bf39de1ce87913a8d0790f5eb835796e023c076
Daniel Oliveira Rangel <rangeldor@gmail.com>
2025-03-12 16:17:05 -0300

    feat: add svg transformer and logo

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,23 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
   
 const config = getDefaultConfig(__dirname);
+
+const { transformer, resolver } = config;
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
   
 module.exports = withNativeWind(config, { input: './global.css' });
--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -2,16 +2,28 @@ import { VStack } from "@components/ui/vstack";
 import { Image } from "@components/ui/image";
 
 import BackgroundImg from '@assets/background.png';
+import Logo from '@assets/logo.svg'
+
+import { Center } from "@components/ui/center";
+import { Text } from "@components/ui/text";
 
 export function SignIn() {
     return (
-        <VStack className="flex-1">
+        <VStack className="flex-1 ">
             <Image
                 source={BackgroundImg}
                 defaultSource={BackgroundImg}
                 className="absolute w-full h-[624]"
                 alt="Pessoas treinando"
             />
+
+            <Center className="my-24">
+                <Logo />
+
+                <Text className="color-gray-100 text-sm">
+                    Treine sua mente e o seu corpo.
+                </Text>
+            </Center>
         </VStack>
     )
 }
