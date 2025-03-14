75fc8f883aa86e4efe3c3078ba8c10345c295893
Daniel Oliveira Rangel <rangeldor@gmail.com>
2025-03-14 15:27:09 -0300

    feat: add routes

---
+++ routes/index.tsx
@@ -0,0 +1,20 @@
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
+import { AuthRoutes } from './auth.routes';
+import { gray } from 'tailwindcss/colors';
+import { ModeType } from '@components/ui/gluestack-ui-provider/types';
+
+export function Routes({ mode = 'dark' }: { mode?: ModeType; }) {
+    const theme = {
+        ...DefaultTheme,
+        colors: {
+            ...DefaultTheme.colors,
+            background: mode === 'dark' ? gray[900] : gray[200],
+        }
+    };
+
+    return (
+        <NavigationContainer theme={theme}>
+            <AuthRoutes />
+        </NavigationContainer>
+    );
+}
