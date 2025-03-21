4da9b93a51eb092f23a6cca9ed720d197a02c744
Daniel Oliveira Rangel <rangeldor@gmail.com>
2025-03-21 17:28:38 -0300

    feat: create auth context and hook

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, ReactNode, useState } from "react";
+
+import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
+
+export interface AuthContextDataProps {
+    user: UserDTO;
+    signIn: (email: string, password: string) => Promise<void>
+}
+
+interface AuthContextProviderProps {
+    children: ReactNode
+}
+
+export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+    const [user, setUser] = useState<UserDTO>({} as UserDTO)
+
+    const signIn = async (email: string, password: string) => {
+        try {
+            const { data } = await api.post('/sessions', { email, password })
+
+            if (data.user) {
+                setUser(data.user)
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, signIn }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
