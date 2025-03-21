89f0a985f87f9b460b3e3d8871933ee824e06d19
Daniel Oliveira Rangel <rangeldor@gmail.com>
2025-03-21 17:28:54 -0300

    feat: add auth hook

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,9 @@ import { AuthRoutes } from './auth.routes';
 import { gray } from 'tailwindcss/colors';
 import { ModeType } from '@components/ui/gluestack-ui-provider/types';
 import { AppRoutes } from './app.routes';
+import { useContext } from 'react';
+import { AuthContext } from '@contexts/AuthContext';
+import { useAuth } from '@hooks/useAuth';
 
 export function Routes({ mode = 'dark' }: { mode?: ModeType; }) {
     const theme = {
@@ -12,6 +15,10 @@ export function Routes({ mode = 'dark' }: { mode?: ModeType; }) {
             background: mode === 'dark' ? gray[950] : gray[200],
         }
     };
+
+    const { user } = useAuth()
+
+    console.log("USUÁRIO LOGADO =>", user);
 
     return (
         <NavigationContainer theme={theme}>
